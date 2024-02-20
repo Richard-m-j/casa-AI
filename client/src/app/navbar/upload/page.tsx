c6b3ffd64263e73e965e4a72331a54364605b68d
Akhil Vettical <107746909+AkhilVettical@users.noreply.github.com>
2024-02-20 22:15:17 +0530

    Drop Zone button added
    upload images and get 3d plan ui

---
+++ client/src/app/navbar/upload/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { MantineProvider } from '@mantine/core'
+import { DropzoneButton } from '@/components/DropzoneButton'
+import { ImageViewer } from '@/components/ImageViewer'
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <MantineProvider>
+        <div className="grid grid-cols-2 gap-8">
+          <DropzoneButton />
+          <ImageViewer />
+        </div>
+      </MantineProvider>
+    </div>
+  );
+}
+
+export default page;
