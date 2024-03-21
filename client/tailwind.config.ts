062e28ccb50576fb3355f1fffe81642118c40b95
Akhil Vettical <107746909+AkhilVettical@users.noreply.github.com>
2024-03-21 19:18:08 +0530

    From Scratch

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "tailwindcss";
 
-const withMT =require("@material-tailwind/react/utils/withMT")
-
-module.exports = withMT({
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",],
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -16,4 +16,5 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+};
+export default config;
